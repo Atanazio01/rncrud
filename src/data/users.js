9c11137f633212250da7b6d87bf1cb8d1ab2f99e
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-22 17:24:20 -0300

    Creating a user file

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar',
+    email: 'riamasil@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2021/09/30/08/53/woman-6669461_960_720.png',
+  },
+  {
+    id: 3,
+    name: 'Julio Fragoso',
+    email: 'juliafrall@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'rafamontee@empmail.com',
+    avatarUrl:
+      'https://cdn.pixabay.com/photo/2021/01/18/08/32/naruto-5927441_960_720.png',
+  },
+];
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
+import {ListItem, Avatar} from 'react-native-elements';
+import users from '../data/users';
 
 export default props => {
-  return <Text>UserList</Text>;
+  const keyExtractor = (item, index) => index.toString();
+  const getUserItem = ({item: user}) => (
+    <ListItem
+      bottomDivider
+      onPress={() => props.navigation.navigate('UserForm')}>
+      <Avatar source={{uri: user.avatarUrl}} />
+      <ListItem.Content>
+        <ListItem.Title>{user.name}</ListItem.Title>
+        <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  );
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={keyExtractor}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
